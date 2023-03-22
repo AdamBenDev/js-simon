bcf527564b774afed006513e01626dcff9becc3e
AdamBenDev <adamaya9732@gmail.com>
2023-03-22 01:11:18 +0100

    Generate function

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 var numeri = [];
 var interval;
+
+
+
+function genera() {
+    numeri = Array.from({ length: 5 }, () => Math.floor(Math.random() * 10));
+    document.getElementById("input").value = numeri.join(" ");
+    interval = setTimeout(() => {
+      document.getElementById("input").value = "";
+      numeri = [];
+    }, 30000);
+  }
