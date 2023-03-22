bfb8b3743fbb80e2067f1f2e2a91ad2fb8f4b07e
AdamBenDev <adamaya9732@gmail.com>
2023-03-22 10:38:22 +0100

    Comments

--- main.js
+++ main.js
@@ -13,10 +13,16 @@ var interval;
 
 
 //genera un array di 5 numeri casuali compresi tra 0 e 9 utilizzando il metodo "Array.from" e "Math.random"
+
+
 function genera() {
     numeri = Array.from({ length: 5 }, () => Math.floor(Math.random() * 10));
     document.getElementById("input").value = numeri.join(" ");
+
+
     //L'intervallo di tempo di 30 secondi viene impostato utilizzando la funzione "setTimeout", dopo il quale l'input dell'utente viene ripulito e l'array "numeri" viene resettato.
+
+
     interval = setTimeout(() => {
       document.getElementById("input").value = "";
       numeri = [];
@@ -24,8 +30,16 @@ function genera() {
   }
 
   //La funzione "controlla()" viene chiamata quando l'utente invia i numeri indovinati
+
+
   function controlla() {
+    //rimuove gli spazi vuoti all'inizio e alla fine della stringa utilizzando il metodo "trim()"
     var input = document.getElementById("input").value.trim();
+
+
+    // il metodo "filter" e gli elementi corrispondenti sono conservati in un nuovo array "indovinati", che viene poi ordinato in ordine crescente utilizzando il metodo "sort()"
+
+    
     var indovinati = numeri.filter((n) => input.includes(n)).sort();
     document.getElementById("risultato").textContent = `Hai indovinato ${indovinati.length} numeri: ${indovinati.join(", ")}`;
     clearTimeout(interval);
