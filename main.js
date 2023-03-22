8805207c3408a2e1927d51853b69c6c78d2ae54e
AdamBenDev <adamaya9732@gmail.com>
2023-03-22 01:29:48 +0100

    function control

--- main.js
+++ main.js
@@ -11,3 +11,13 @@ function genera() {
       numeri = [];
     }, 30000);
   }
+
+  
+  function controlla() {
+    var input = document.getElementById("input").value.trim();
+    var indovinati = numeri.filter((n) => input.includes(n)).sort();
+    document.getElementById("risultato").textContent = `Hai indovinato ${indovinati.length} numeri: ${indovinati.join(", ")}`;
+    clearTimeout(interval);
+    numeri = [];
+    document.getElementById("input").value = "";
+  }
